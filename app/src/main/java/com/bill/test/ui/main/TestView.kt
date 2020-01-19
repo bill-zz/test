e0ce133dfb02f2ea6b4013496f2c52b54f8b07d4
bill <751309401@qq.com>
2020-01-19 16:05:32 +0800

    Initial commit

---
+++ app/src/main/java/com/bill/test/ui/main/TestView.kt
@@ -0,0 +1,18 @@
+package com.bill.test.ui.main
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import java.util.jar.Attributes
+
+class TestView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    init {
+
+    }
+
+}
